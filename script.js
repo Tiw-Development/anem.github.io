4ee8c19dab36792c78a117bb7946eb2bbd7e9ff6
Tiw <115408072+Tiw-Development@users.noreply.github.com>
2024-09-26 02:22:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,292 +1,89 @@
-const themeToggle = document.getElementById('theme-toggle');
-const body = document.body;
-const moon = document.querySelector('.fa-moon');
-const info = document.querySelector('.info-container');
-const login = document.querySelector('.login-container');
-const rows = document.querySelectorAll('.info-table tr');
-
-
-if (localStorage.getItem('theme') === 'dark') {
-    body.classList.add('dark-mode');
-    info.classList.add('container-dark');
-    login.classList.add('container-dark');
-    moon.style = "color: #fdfdfd";
-    themeToggle.checked = true;
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>فحص مواعيد</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+</head>
+<body>
+    <div class="page-border">
+        <div class="switch-container">
+            <label class="switch">
+                <input type="checkbox" id="theme-toggle">
+                <span class="slider">
+                    <i class="fa-solid fa-sun icon"></i>
+                    <i class="fa-solid fa-moon icon"></i>
+                </span>
+            </label>
+        </div>
+    </div>
+    <div class="login-container">
+       
+        <div class="loginFormCont">
+            <div class="login-border">
+                <h2>التسجيل</h2>
+            </div>
+            <form id="loginForm">
+                <div class="input-group">
+                    <label for="NumWassit">رقم بطاقة طالب العمل</label>
+                    <input type="text" id="NumWassit" name="NumWassit" placeholder="رقم بطاقة طالب العمل" required>
+                </div>
+                <div class="input-group">
+                    <label for="natNumID">رقم التعريف الوطني / رقم بطاقة التعريف</label>
+                    <input type="text" id="natNumID" name="natNumID" placeholder="رقم التعريف الوطني / رقم بطاقة التعريف" required>
+                </div>
+                <div id="notify-login">
+                    <p id="message-login"></p>
+                </div>
+                <button class="envoyebtn" type="submit">إرسـال</button>
+            </form>
+        </div>
+    </div>
     
-    rows.forEach(row => {
-    
-        const firstCell = row.querySelector('td:first-child');
-        const lastCell = row.querySelector('td:last-child');
-        if (firstCell && lastCell) {
-            firstCell.style = 'background-color: rgb(60 139 117 / 42%); color: #ffffffc7'; 
-            lastCell.style = 'background-color: rgb(28 28 28); color: #ffffffc7'; 
-        }
-    });
-}
 
-themeToggle.addEventListener('change', () => {
-    if (themeToggle.checked) {
-        body.classList.add('dark-mode');
-        info.classList.add('container-dark');
-        login.classList.add('container-dark');
-        moon.style = "color: #fdfdfd";
+    <div class="info-container" style="display:none;">
+        <div class="info-border">
+          <h2>معلوماتك</h2>
+        </div>
+        <div id="notify-checked">
+            <p id="message-checked"></p>
+        </div>
+        <div class="info-table-border">
+        <table class="info-table">
+            <tbody class="info-table-body">
+              <tr>
+                <td>اللقب </td>
+                <td id="displayNom"></td>
+              </tr>
+              <tr>
+                <td>الاسم </td>
+                <td id="displayPrenom"></td>
+              </tr>
+              <tr>
+                <td>رقم بطاقة طالب العمل</td>
+                <td id="displayNatNumID"></td>
+              </tr>
+              <tr>
+                <td>المقابلة مبرمجة على مستوى وكالة	</td>
+                <td id="displayAgence"></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div id="notify-info">
+            <p id="message-info"></p>
+        </div>
+        <div id="notify-info-info">
+            <p id="message-info-info" style="margin-block-end: -0.6em;"></p>
+        </div>
+        <button class="checkRend">ابدا البحث</button>
+        <button class="HideInfo">إخفاء معلوماتي</button>
+        <button class="logout">تسجيل الخروج</button>
+    </div>
 
-    rows.forEach(row => {
-    
-        const firstCell = row.querySelector('td:first-child');
-        const lastCell = row.querySelector('td:last-child');
-        if (firstCell && lastCell) {
-            firstCell.style = 'background-color: rgb(60 139 117 / 42%); color: #ffffffc7'; 
-            lastCell.style = 'background-color: rgb(28 28 28); color: #ffffffc7'; 
-        }
-    });
-        localStorage.setItem('theme', 'dark');
-        login.classList.add('container-dark');
-    } else {
-        body.classList.remove('dark-mode');
-        info.classList.remove('container-dark');
-        login.classList.remove('container-dark');
-        moon.style = "color: #8f8b8b";
-
-
-        rows.forEach(row => {
-    
-            const firstCell = row.querySelector('td:first-child');
-            const lastCell = row.querySelector('td:last-child');
-            if (firstCell && lastCell) {
-                firstCell.style = 'background-color: #dbdbdb; color: #519985'; 
-                lastCell.style = 'background-color: #fff; color: #0a0a0ac7'; 
-            }
-        });
-        localStorage.setItem('theme', 'light');
-
-    }
-});
-
-function checkSession() {
-    const storedNumWassit = localStorage.getItem('NumWassit');
-    const storedNatNumID = localStorage.getItem('natNumID');
-
-    if (storedNumWassit && storedNatNumID) {
-
-        document.querySelector('.info-container').style.display = 'block';
-        document.querySelector('.login-container').style.display = 'none';
-        document.getElementById("displayNom").textContent = localStorage.getItem('Nom');
-        document.getElementById("displayPrenom").textContent = localStorage.getItem('Prenom');
-        document.getElementById("displayNatNumID").textContent = storedNumWassit;
-        document.getElementById("displayAgence").textContent = localStorage.getItem('Agence');
-    }
-}
-
-
-checkSession();
-
-async function checkRendeVous() {
-    const storedStructureid = localStorage.getItem('structureIdStored');
-    const storedPreInscriptionId = localStorage.getItem('preInscriptionIdStored');
-
-    const Url_dates = `https://ac-controle.anem.dz/AllocationChomage/api/RendezVous/GetAvailableDates?StructureId=${storedStructureid}&PreInscriptionId=${storedPreInscriptionId}`;
-    
-    try {
-        const response = await axios.get(Url_dates);
-        const Data = response.data;
-
-        
-        if (Data && Data.dates) {
-            const availableDates = Data.dates || [];
-            if (availableDates.length > 0) {
-                const firstDate = availableDates[0];
-                const lastDate = availableDates[availableDates.length - 1];
-                document.getElementById("message-checked").textContent = `📅 تم العثور على  ${availableDates.length} موعد متاحة ☑️ `;
-                document.getElementById("notify-checked").style.display = "block";
-                if (localStorage.getItem('theme') === 'dark') {
-                    document.getElementById("displayAgence").style = "background-color: #3cff1847; color: rgba(255, 255, 255, 0.78)";
-                   } else {
-                    document.getElementById("displayAgence").style = "background-color: #3cff1847; color: rgba(10, 10, 10, 0.78)";
-                    }
-
-                document.getElementById("notify-info").style.display = "block";
-                document.getElementById("message-info").style.paddingTop = "1%";
-                document.getElementById("message-info").textContent = `📅 من ${firstDate} الى ${lastDate} 📅`;
-                document.getElementById("notify-info").style = "background-color: #207444";
-            } else {
-              
-                document.getElementById("message-info").textContent = "لا توجد مواعيد متاحة.";
-                document.getElementById("notify-info").style.display = "block";
-               document.getElementById("message-info").style.paddingTop = "2%";
-               if (localStorage.getItem('theme') === 'dark') {
-                document.getElementById("displayAgence").style = "background-color: #ff00003d; color: rgba(255, 255, 255, 0.78)";
-               } else {
-                document.getElementById("displayAgence").style = "background-color: #ff00003d; color: rgba(10, 10, 10, 0.78)";
-                }
-            }
-        } else {
-            document.getElementById("message-info").textContent = "حدث خطأ في الاتصال بالخادم.";
-            document.getElementById("notify-info").style.display = "block";
-           document.getElementById("message-info").style.paddingTop = "2%";
-        }
-    } catch (error) {
-        console.error("حدث خطأ أثناء جلب المواعيد:", error);
-        document.getElementById("message-info").textContent = "حدث خطأ في الاتصال بالخادم.";
-        document.getElementById("notify-info").style.display = "block";
-    }
-}
-
-
-document.getElementById("loginForm").addEventListener("submit", async function(event) {
-    event.preventDefault();
-
-    var NumWassit = document.getElementById("NumWassit").value;
-    var natNumID = document.getElementById("natNumID").value;
-
-    const url = `https://ac-controle.anem.dz/AllocationChomage/api/validateCandidate/query?wassitNumber=${NumWassit}&identityDocNumber=${natNumID}`;
-
-    try {
-        const response = await axios.get(url);
-        const data = response.data;
-
-        const preInscriptionId = data.preInscriptionId;
-        const havePreInscription = data.havePreInscription;
-
-        if (havePreInscription && preInscriptionId) {
-            const url2 = `https://ac-controle.anem.dz/AllocationChomage/api/PreInscription/GetPreInscription?Id=${preInscriptionId}`;
-            const response2 = await axios.get(url2);
-            const data2 = response2.data;
-            const Nom = data2.nomDemandeurAr;
-            const Prenom = data2.prenomDemandeurAr;
-            const Agence = data2.structureAr;
-
-           
-            localStorage.setItem('NumWassit', NumWassit);
-            localStorage.setItem('natNumID', natNumID);
-            localStorage.setItem('Nom', Nom);
-            localStorage.setItem('Prenom', Prenom);
-            localStorage.setItem('Agence', Agence);
-
-            
-            document.querySelector('.info-container').style.display = 'block';
-            document.querySelector('.login-container').style.display = 'none';
-            document.getElementById("displayNom").textContent = Nom;
-            document.getElementById("displayPrenom").textContent = Prenom;
-            document.getElementById("displayNatNumID").textContent = NumWassit;
-            document.getElementById("displayAgence").textContent = Agence;
-
-            const structureId = data2.structureId;
-            if (structureId) {
-                const url3 = `https://ac-controle.anem.dz/AllocationChomage/api/RendezVous/GetAvailableDates?StructureId=${structureId}&PreInscriptionId=${preInscriptionId}`;
-                const response3 = await axios.get(url3);
-                const data3 = response3.data;
-                localStorage.setItem('structureIdStored', structureId);
-                localStorage.setItem('preInscriptionIdStored', preInscriptionId);
-                const availableDates = data3.dates || [];
-                if (availableDates.length > 0) {
-                    const firstDate = availableDates[0];
-                    const lastDate = availableDates[availableDates.length - 1];
-                    document.getElementById("message-checked").textContent = `📅 تم العثور على  ${availableDates.length} موعد متاحة ☑️ `;
-                    document.getElementById("displayAgence").style = "background-color: #3cff1847; color: rgba(255, 255, 255, 0.78)";
-                    document.getElementById("notify-checked").style.display = "block";
-
-                    document.getElementById("notify-info").style.display = "block";
-                    document.getElementById("message-info").style.paddingTop = "1%";
-                    document.getElementById("message-info").textContent = `📅 من ${firstDate} الى ${lastDate} 📅`;
-                    document.getElementById("notify-info").style = "background-color: #207444";
-                } else {
-                    document.getElementById("message-info").textContent = "لا توجد مواعيد متاحة.";
-                    document.getElementById("notify-info").style.display = "block";
-                    document.getElementById("message-info").style.paddingTop = "2%";
-                }
-            } else {
-                document.getElementById("message-login").textContent = "حدث مشكلة ما , حاول مرة اخرى !";
-                document.getElementById("notify-login").style.display = "block";
-                localStorage.clear();
-                document.querySelector('.info-container').style.display = 'none';
-                document.querySelector('.login-container').style.display = 'block';
-            }
-        } else {
-            document.getElementById("message-login").textContent = "الرجاء إدخال معلومات صحيحة!";
-            document.getElementById("notify-login").style.display = "block";
-        }
-    } catch (error) {
-        console.error("حدث خطأ أثناء جلب البيانات:", error);
-        document.getElementById("message-login").textContent = "حدث خطأ في الاتصال بالخادم.";
-        document.getElementById("notify-login").style.display = "block";
-    }
-
-    setTimeout(function() {
-        document.getElementById("notify-login").style.display = "none";
-    }, 8000);
-});
-
-
-let infoVisible = true; 
-let searching  = true;
-let intervalId; 
-let countdownInterval;
-let searchCount = 0;
-let countdown = 20; 
-
-function updateCountdown() {
-    document.getElementById("message-info-info").innerHTML = `الوقت المتبقي لإعادة البحث ${countdown} ثانية<br> تم البحث ${searchCount} مرة`;
-    document.getElementById("notify-info-info").style.display = "block";
-    countdown--;
-
-    if (countdown < 0) {
-        countdown = 20;
-        searchCount++;
-        document.getElementById("message-info-info").innerHTML = `الوقت المتبقي لإعادة البحث ${countdown} ثانية <br> تم البحث ${searchCount} مرة`;
-        document.getElementById("notify-info-info").style.display = "block";
-        checkRendeVous();
-    }
-}
-
-document.querySelector('.HideInfo').addEventListener('click', function() {
-    if (infoVisible) {
-
-        document.getElementById("displayNom").textContent = "مخفي";
-        document.getElementById("displayPrenom").textContent = "مخفي";
-        document.getElementById("displayNatNumID").textContent = "مخفي";
-        document.querySelector('.HideInfo').textContent = 'إظهار معلوماتي';
-        document.querySelector('.HideInfo').style = 'background-color: rgb(205 42 42)';
-    } else {
-
-        document.getElementById("displayNom").textContent = localStorage.getItem('Nom');
-        document.getElementById("displayPrenom").textContent = localStorage.getItem('Prenom');
-        document.getElementById("displayNatNumID").textContent = localStorage.getItem('NumWassit');
-        document.querySelector('.HideInfo').textContent = 'إخفاء معلوماتي';
-        document.querySelector('.HideInfo').style = 'background-color: #0c38b5de';
-    }
-
-
-    infoVisible = !infoVisible;
-});
-
-
-document.querySelector('.logout').addEventListener('click', function() {
-    localStorage.clear();
-    document.querySelector('.info-container').style.display = 'none';
-    document.querySelector('.login-container').style.display = 'block';
-});
-
-document.querySelector('.checkRend').addEventListener('click', function() {
-    if (searching) {
-        document.querySelector('.checkRend').textContent = 'اوقف البحث'
-        document.querySelector('.checkRend').style = 'background-color: rgb(205 42 42)';
-        intervalId = setInterval(checkRendeVous, 20000);
-        countdownInterval = setInterval(updateCountdown, 1000);
-
-    } else {
-        clearInterval(intervalId);
-        clearInterval(countdownInterval);
-        document.querySelector('.checkRend').textContent = 'ابدأ البحث'
-        document.querySelector('.checkRend').style = 'background-color: rgb(34 179 65)';
-        document.getElementById("notify-info").style.display = "none";
-        countdown = 20; 
-        searchCount = 0
-        document.getElementById("message-info-info").innerHTML = `الوقت المتبقي لإعادة البحث ${countdown} ثانية <br> تم البحث${searchCount} مرة`;
-    }
-
-    searching  = !searching ;
-});
-
-
-checkSession();
+    <script src="script.js"></script>
+</body>
+</html>
